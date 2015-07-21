3b9cda1a056cf8dc15f922e8250c9ff49b76e2aa
Urs Gürtler <u.guertler@fusion7.ch>
2015-07-21 11:32:51 +0200

    Templating stuff

--- server.js
+++ server.js
@@ -6,7 +6,16 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 
 // setup view engine
-var handlebars = require('express3-handlebars').create({defaultLayout: 'main'});
+var handlebars = require('express3-handlebars').create({
+	defaultLayout: 'main',
+	helpers: {
+		section: function(name, options) {
+			if(!this._sections) this._sections = {};
+			this._sections[name] = options.fn(this);
+			return null;
+		}
+	}
+});
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
